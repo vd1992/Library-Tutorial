3524b54e281681a6fb3bc98c0e5265c3db4ef030
vd1992 <40485260+vd1992@users.noreply.github.com>
2020-11-10 00:21:58 -0700

    Initial commit

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -107,7 +107,7 @@ exports.author_delete_get = function(req, res, next) {
 
 }; 
 
-// Handle Author delete on POST 
+// Handle Author delete on POST. 
 exports.author_delete_post = function(req, res, next) {
 
     async.parallel({
